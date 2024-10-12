5b7d01fd256d4345526aab6983b4997dd244166d
ryanbuck182@ <ryanbuck182@gmail.com>
2024-10-12 18:25:54 -0400

    Add input validation for name

--- MyFirstApplication/Pages/Index.cshtml.cs
+++ MyFirstApplication/Pages/Index.cshtml.cs
@@ -11,11 +11,17 @@ namespace MyFirstApplication.Pages {
         public string emailAddress { get; set; }
 
         [BindProperty]
+        [Required(ErrorMessage = "First name is required.")]
+        [MaxLength(14, ErrorMessage = "First name is too long. It must be at most 14 characters.")]
+        [MinLength(2, ErrorMessage = "First name is too short. It must be at least 2 characters.")]
+        public string firstNameInput { get; set; }
+        
+        [BindProperty]
         [Required(ErrorMessage = "Email address is required.")]
         [MaxLength(254, ErrorMessage = "Email address is too long. It must be at most 254 characters.")]
         [MinLength(5, ErrorMessage = "Email address is too short. It must be at least 5 characters.")]
-        [RegularExpression("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", //credit: https://emailregex.com/index.html
-            ErrorMessage = "Email address does not fit standards. This may be due to invalid special characters (only !#$%&'*+/=?^_`|~- are allowed). It may also be due to invalid structure, proper structure would be name@domain.")]
+        [RegularExpression("[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*", //credit: https://emailregex.com/index.html
+            ErrorMessage = "Email address does not fit standards. This may be due to invalid special characters (only !#$%&'*+/=?^_`|~- are allowed, and only in the name portion). It may also be due to invalid structure, proper structure would be name@domain.")]
         public string emailAddressInput { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger) {
@@ -33,7 +39,7 @@ namespace MyFirstApplication.Pages {
                 return Page();
             }
 
-            this.firstName = Request.Form["firstName"];
+            this.firstName = Request.Form["firstNameInput"];
             this.emailAddress = Request.Form["emailAddressInput"];
 
             return Page();
